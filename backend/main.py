9b64063af0fd92b6ac02b7d529cc84b5bf1c5a37
Viswa <viswasabari9211@gmail.com>
2026-06-05 16:37:57 +0530

    Added gunicorn

--- backend/main.py
+++ backend/main.py
@@ -131,4 +131,12 @@ if __name__ == "__main__":
         host="127.0.0.1",
         port=5000,
         debug=True
+    )import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(
+        host="0.0.0.0",
+        port=port
     )
